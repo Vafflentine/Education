792d1c5da5a7c0fcad0e94d6324e1c8a072e3a70
Vafflentine <kartoshkapink@gmail.com>
2021-08-09 22:16:35 +0300

    Create function to get posts sync

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func getPosts(url string) []byte {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return body
+}
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println(string(getPosts("https://jsonplaceholder.typicode.com/posts")))
 }
